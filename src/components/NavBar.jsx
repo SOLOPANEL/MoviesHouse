7e01e0d90f9ce4bfbcabf0efa24087600e9326cb
SOLOPANEL <red8carbon@gmail.com>
2026-05-15 19:48:37 +0530

    Fix: Correct Navbar.css case sensitivity

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom" 
-import "../css/NavBar.css"
+import { Link } from "react-router-dom"
+import "../css/Navbar.css"
 
 function NavBar() {
-    return(
+    return (
         <nav className="navbar">
             <div className="navbar-brand">
                 <Link to="/">Movie App</Link>
